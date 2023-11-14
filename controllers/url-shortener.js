86d83e6a056e0413df01a148fc12dabf9eb444f3
Mahmood, Daniyal <daniyal.mahmood@lowes.com>
2023-11-14 11:16:18 +0530

    fix redirects

--- controllers/url-shortener.js
+++ controllers/url-shortener.js
@@ -6,25 +6,28 @@ const dns = require('dns')
 
 const idUrlMap = new Map()
 const urlIdMap = new Map()
+let id = 0
 
 router.post("/shorturl", (req, res) => {
     try {
-        let id
         let { url: original_url } = req.body
-        let url = original_url.replace(new RegExp("https*://"), "")
-        if (urlIdMap.has(url)) return res.json({ original_url, short_url: urlIdMap.get(url) })
+        let url = new URL(original_url).origin
+        url = url.replace(new RegExp("https*://"), "")
+        if (urlIdMap.has(original_url)) return res.json({ original_url, short_url: urlIdMap.get(original_url) })
 
         dns.lookup(url, (err, address) => {
             if (err) {
                 console.log(err);
                 return res.json({ error: "Invalid URL" })
             }
-            id = crypto.randomBytes(5).toString('hex')
-            while (idUrlMap.has(id)) {
-                id = crypto.randomBytes(5).toString('hex')
-            }
-            idUrlMap.set(id, url)
-            urlIdMap.set(url, id)
+            // id = crypto.randomBytes(5).toString('hex')
+            // while (idUrlMap.has(id)) {
+            //     id = crypto.randomBytes(5).toString('hex')
+            // }
+            id++
+            idUrlMap.set(id, original_url)
+            urlIdMap.set(original_url, id)
+            console.log(idUrlMap);
             return res.json({ original_url, short_url: id })
         })
 
@@ -35,9 +38,9 @@ router.post("/shorturl", (req, res) => {
 
 })
 
-router.get("/shorturl/:id", (req, res)=>{
-    const { id } = req.params
-    if(idUrlMap.has(id)) return res.redirect("http://"+idUrlMap.get(id))
+router.get("/shorturl/:short", (req, res)=>{
+    const { short } = req.params
+    if(idUrlMap.has(id)) return res.redirect(idUrlMap.get(parseInt(short)))
     else return res.json({error:" Invalid shorturl"})
 })
 
