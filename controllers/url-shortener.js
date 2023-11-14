cc08a017dcccdd244e53cfd59ddccf81e88d3d73
Mahmood, Daniyal <daniyal.mahmood@lowes.com>
2023-11-14 11:02:53 +0530

    fix redirects

--- controllers/url-shortener.js
+++ controllers/url-shortener.js
@@ -4,29 +4,41 @@ const crypto = require('crypto')
 const router = require('express').Router()
 const dns = require('dns')
 
-const addressSet = new Map()
+const idUrlMap = new Map()
+const urlIdMap = new Map()
 
 router.post("/shorturl", (req, res) => {
     try {
         let id
-        dns.lookup(req.body.url, (err, address)=>{
-            if(err) {
+        let { url: original_url } = req.body
+        let url = original_url.replace(new RegExp("https*://"), "")
+        if (urlIdMap.has(url)) return res.json({ original_url, short_url: urlIdMap.get(url) })
+
+        dns.lookup(url, (err, address) => {
+            if (err) {
                 console.log(err);
-                return res.json({error: "Invalid URL"})
+                return res.json({ error: "Invalid URL" })
             }
             id = crypto.randomBytes(5).toString('hex')
-            while(addressSet.has(id)){
+            while (idUrlMap.has(id)) {
                 id = crypto.randomBytes(5).toString('hex')
             }
-            addressSet.set(id, address)
-            return res.json({original_url: req.body.url, short_url: id})
-        }) 
-        
+            idUrlMap.set(id, url)
+            urlIdMap.set(url, id)
+            return res.json({ original_url, short_url: id })
+        })
+
     } catch (error) {
         console.log(error);
-        return res.json({error: "Invalid URL"})
+        return res.json({ error: "Invalid URL" })
     }
-    
+
 })
 
-module.exports =  router
+router.get("/shorturl/:id", (req, res)=>{
+    const { id } = req.params
+    if(idUrlMap.has(id)) return res.redirect("http://"+idUrlMap.get(id))
+    else return res.json({error:" Invalid shorturl"})
+})
+
+module.exports = router
